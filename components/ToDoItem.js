be558d2bb5d34fde257ef1c24a202a595004d794
Angel R <angelzrod4@gmail.com>
2022-01-07 15:53:40 -0800

    able to add todo inside of buckets

---
+++ components/ToDoItem.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Colors from "../colors/Colors";
+
+export default ({ text, isChecked }) => {
+  return <Text>{text}</Text>;
+};
--- containers/ToDoList.js
+++ containers/ToDoList.js
@@ -1,10 +1,55 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React, { useState, useLayoutEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import ToDoItem from "../components/ToDoItem";
 import Colors from "../colors/Colors";
 
-export default () => {
-  return <View style={styles.container}></View>;
+const renderAddIcon = (addTodo) => {
+  return (
+    <TouchableOpacity
+      onPress={() => addTodo({ text: "hello2", isChecked: false })}
+    >
+      <Text style={styles.icon}>+ </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ({ navigation }) => {
+  const [toDoItems, setToDoItems] = useState([
+    { text: "Hello", isChecked: false },
+  ]);
+
+  const addTodo = (todo) => {
+    toDoItems.push(todo);
+    setToDoItems([...toDoItems]);
+  };
+
+  const removeTodo = (index) => {
+    toDoItems.splice(index, 1);
+    setToDoItems([...toDoItems]);
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => renderAddIcon(addTodo),
+    });
+  });
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={toDoItems}
+        renderItem={({ item: { text, isChecked }, index }) => {
+          return <ToDoItem text={text} isChecked={isChecked} />;
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
