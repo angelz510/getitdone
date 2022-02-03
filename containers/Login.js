9b651aa85d5dd722440a51faae6556b43bac5f58
Angel R <angelzrod4@gmail.com>
2022-02-02 16:12:07 -0800

    build(login):add and style email and pw input fields

--- containers/Login.js
+++ containers/Login.js
@@ -1,17 +1,54 @@
-import React, { useLayoutEffect, useState } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import Colors from "../colors/Colors";
 import Button from "../components/Button";
+import LabeledInput from "../components/LabeledInput";
 
 export default () => {
   const [isCreateMode, setIsCreateMode] = useState(true);
+  const [emailField, setEmailField] = useState({ text: "", errorMessage: "" });
+  const [passwordField, setPasswordField] = useState({
+    text: "",
+    errorMessage: "",
+  });
+  const [passwordConfirmationField, setPasswordConfirmationField] = useState({
+    text: "",
+    errorMessage: "",
+  });
   return (
     <View style={styles.container}>
       <Text style={styles.header}>📝 Get It Done!</Text>
       <View style={{ flex: 1 }}>
-        {/* Email input */}
-        {/* password input */}
-        {/* password confirmation */}
+        <LabeledInput
+          label="Email"
+          text={emailField.text}
+          onChangeText={(text) => {
+            setEmailField({ text });
+          }}
+          errorMessage={emailField.errorMessage}
+          labelStyle={styles.label}
+          autoCompleteType="email"
+        />
+        <LabeledInput
+          label="Password"
+          text={passwordField.text}
+          onChangeText={(text) => {
+            setPasswordField({ text });
+          }}
+          errorMessage={passwordField.errorMessage}
+          labelStyle={styles.label}
+          autoCompleteType="password"
+        />
+        <LabeledInput
+          label="Re-enter Password"
+          text={passwordConfirmationField.text}
+          onChangeText={(text) => {
+            setPasswordConfirmationField({ text });
+          }}
+          errorMessage={passwordConfirmationField.errorMessage}
+          labelStyle={styles.label}
+          autoCompleteType="password"
+        />
         {/* login toggle */}
       </View>
       <Button
